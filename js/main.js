cddc383f736bc726d39cd6ed1992a0845cabbbb2
vovgreba <49303544+vovgreba@users.noreply.github.com>
2020-06-20 12:59:14 +0300

    solutions to the third lesson

--- js/main.js
+++ js/main.js
@@ -1,36 +1,69 @@
 // ------- required task ----------
 
-let money = 20000,
+let money = +prompt('Ваш месячный доход?'),
     income = '25000',
-    addExpenses = 'Internet, Taxi, Shopping',
-    deposit = true,
+    addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую?'),
+    deposit = Boolean(confirm('Есть ли у вас депозит в банке? Если есть нажмите OK, если нету то нажмите cansel')),
     mission = 50000,
-    period = 12;
+    period = 12,
+    expenses1 =+prompt('Введите обязательную статью расходов?'),
+    expenses2 =+prompt('Введите обязательную статью расходов?'),
+    amount1 =+prompt('Во сколько это обойдется?'),
+    amount2 =+prompt('Во сколько это обойдется?'),
+    budgetMonth = money - (expenses1 + expenses2) - (amount1 + amount1),
+    missionCompleted = mission / budgetMonth,
+    budgetDay = budgetMonth / 30;
 
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
+console.log(`Бюджет на месяц: ${budgetMonth}`);
+console.log(`Цель будет достигнута за: ${Math.ceil(missionCompleted)}`);
+console.log(`бюджет на день: ${Math.round(budgetDay)}`);
+if (budgetDay > 1200) {
+  console.log("У вас высокый уровень дохода")
+  }else if (600 > budgetDay < 1200 ) {
+    console.log("У вас средный уровень дохода")
+  } else if (budgetDay < 600) {
+    console.log("К сожалению у вас уровень дохода ниже среднего")
+  }else {
+    console.log("Что-то пошло не так")
+}
 
-console.log(addExpenses.length);
-console.log(`"Период равен ${period} месяцев"`);
-console.log(`"Цель заработать ${mission} гривень"`);
 
-let arayExpenses = addExpenses.toLowerCase().split();
-console.log(arayExpenses);
 
- let budgetDay = money / 30;
- console.log(parseInt(budgetDay));
 
-//  -------- hard task -------------
+                                            // -------- hard task -------------
+/*
+let lang = prompt("Введите на каком языке вам показать дни недели: 'ru' или 'en'");
+const weekOfRussian = "Понедельник, вторник, среда, четверг, пятница, суббота, неделя";
+const weekOfEnglish = "Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday";
+if (lang === 'ru') {
+  console.log(`Дни недели на русском: ${weekOfRussian}`);
+} else if (lang === 'en') {
+console.log(`Days of the week in English: ${weekOfEnglish}`);
+} else {
+  console.log("Что то пошло не так, вы ввели неправальние значания!")
+}
+*/
 
- let num = 266219;
- let array = String(num).split("");
+// ------------ делаем все тоже самое только с помомощью конструкции switch -----
+/*
+let lang = prompt("Введите на каком языке вам показать дни недели: 'ru' или 'en'");
+const weekOfRussian = "Понедельник, вторник, среда, четверг, пятница, суббота, неделя";
+const weekOfEnglish = "Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday";
+switch(lang) {
+    case "ru": 
+        console.log(`Дни недели на русском: ${weekOfRussian}`);
+        break;
+    case "en": 
+        console.log(`Days of the week in English: ${weekOfEnglish}`);
+        break;
+    default: 
+        console.log("Что то пошло не так, вы ввели неправальние значания!")
+        break;
+}
+*/
 
- let result = array.reduce(function(sum, current) {
-  return sum * current;
-}, 1);
+// сдесь мы спрашиваем у посетителя его имя, и с помощью тернарного оператора выводим разные значения 
+// let namePerson = prompt("введите ваше имя с большой буквы:");
 
-console.log(result);
-let exponent = result ** 3;
-console.log(exponent.toString().substr(0,2));
-
+// namePerson === "Артем" ? console.log("директор") : namePerson ==="Максим" ? console.log("преподаватель") : console.log("студент");
+ 
