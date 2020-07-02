7ab768655e2f6b3ac94356efa7c202de034f0648
vovgreba <49303544+vovgreba@users.noreply.github.com>
2020-07-02 15:56:40 +0300

    solutions to lesson 5

--- js/main.js
+++ js/main.js
@@ -1,59 +1,87 @@
-// ------- required task ----------
+// // ------- required task ----------
 
-let money = +prompt('Ваш месячный доход?'),
-    income = '25000',
-    addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую?').split(','),
-    deposit = Boolean(confirm('Есть ли у вас депозит в банке? Если есть нажмите OK, если нету то нажмите cansel')),
-    mission = 50000,
-    period = 12,
-    expenses1 =prompt('Введите обязательную статью расходов?', 'Курсы англ.языка'),
-    amount1 =+prompt('Во сколько это обойдется?', 1000),
-    expenses2 =prompt('Введите обязательную статью расходов?', 'Автошкола'),
-    amount2 =+prompt('Во сколько это обойдется?', 1500);
-let showTypeOf = function(data) {
-  console.log(data, typeof(data));
-}
+// let money,
+//     income = '25000',
+//     addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую?'),
+//     deposit = Boolean(confirm('Есть ли у вас депозит в банке? Если есть нажмите OK, если нету то нажмите cansel')),
+//     mission = 50000,
+//     period = 12;
+  
+    
 
-let getExpensesMonth = function() {
-  return amount1 + amount1;
-}
-function calculateDeposit() {
-  if(deposit === true) {
-    return +prompt('Какой у вас депозит?');
-  }
-}
-let valueDeposit = calculateDeposit();
-function getAccumulatedMonth() {
-  if(valueDeposit != undefined) {
-    return money + valueDeposit - (amount1 + amount2) ;
-  } else {
-    return money - (amount1 + amount2);
-  }
-}
-let accumulatedMonth = getAccumulatedMonth(),
-    budgetDay = accumulatedMonth / 30;
-function getTargetMonth() {
-  return mission / accumulatedMonth
-}
-let getStatusIncome = function() {
-  if (budgetDay > 1200) {
-    console.log("У вас высокый уровень дохода")
-    }else if (600 > budgetDay < 1200 ) {
-      console.log("У вас средный уровень дохода")
-    } else if (budgetDay < 600) {
-      console.log("К сожалению у вас уровень дохода ниже среднего")
-    }else {
-      console.log("Что-то пошло не так")
-  }  
-}
-showTypeOf(money);
-showTypeOf(income);
-showTypeOf(addExpenses);
-console.log(`Сума обьязательных рассходов за месяц: ${getExpensesMonth()}`);
-console.log(`Возможные рассходы: ${addExpenses}`);
-console.log(`Цель будет достигнута за: ${Math.ceil(getTargetMonth())} месяцев.`);
-console.log(`бюджет на день: ${Math.round(budgetDay)}`);
-getStatusIncome();
+// let start = function() {
+//   do {
+//     money = +prompt('Ваш месячный доход?');
+//   } while (isNaN(money) || money === '' || money === null);
+// }
+
+// start();
+
+// let expenses = [];
+// let getExpensesMonth = function() {
+//   let sum = 0,
+//       question = 0;
+//   for (let i = 0; i < 2; i++) {
+//       expenses[i] = prompt('Введите обязательную статью расходов?');
+      
+//       do {
+//         question = +prompt('Во сколько это обойдется?');
+//       } while (isNaN(question));
+      
+//       sum += question;
+//     }
+//     return sum;
+//   }
+
+// let expensesAmount = getExpensesMonth();
+
+
+// function calculateDeposit() {
+//   if(deposit === true) {
+//     return +prompt('Какой у вас депозит?');
+//   }
+// }
+
+// let valueDeposit = calculateDeposit();
+
+// function getAccumulatedMonth() {
+//   if(valueDeposit != undefined) {
+//     return money + valueDeposit - expensesAmount ;
+//   } else {
+//     return money - expensesAmount;
+//   }
+// }
+// let accumulatedMonth = getAccumulatedMonth(),
+//     budgetDay = accumulatedMonth / 30;
+
+// let getTargetMonth = function () {
+//   return mission / accumulatedMonth
+// }
+
+// let getTargetMission = function () {
+//   if ( getTargetMonth() > 0 ) {
+//     console.log(`Цель будет достигнута за: ${Math.ceil(getTargetMonth())} месяца.`);
+//   } else {
+//     console.log(`Цель не будет достигнута`);
+//   }
+// }
+// let getStatusIncome = function() {
+//   if (budgetDay > 1200) {
+//     console.log("У вас высокый уровень дохода")
+//     } else if (budgetDay < 1200 && budgetDay > 600 ) {
+//       console.log("У вас средный уровень дохода")
+//     } else if (budgetDay < 600) {
+//       console.log("К сожалению у вас уровень дохода ниже среднего")
+//     } else {
+//       console.log("Что-то пошло не так")
+//   }  
+// }
+
+// console.log(`Сума обьязательных рассходов за месяц: ${expensesAmount}`);
+// console.log(addExpenses.toLowerCase().split(','));
+// console.log(`бюджет на день: ${Math.round(budgetDay)}`);
+// getStatusIncome();
+// getTargetMission();
 
 
 
@@ -61,17 +89,42 @@ getStatusIncome();
 
 
                                             // -------- hard task -------------
-/*
-let stringsMake = function (user) {
-  let stringType = typeof(user);
-  if ( stringType != "string") {
-    console.log(`В качестве аргумента была передана не строка:${user}` );
-  } else {
-    // здесь с помощью метода .trim() убираем пробелы в начали и в конце
-    //а с помощью  метода .substring()показываем только то количоство символом которое входит в диапазон, 
-    // остальное показываем в виде ...
-    console.log(`В качестве аргумента была передана строка:${user.trim().substring(0,31) + "..."}`);
+
+/*********************Здесь мы создаем масив из семи чисел, с помощью цыкла мы по нему пробегаемся, далее с помощю конструкции задаем условия, что если в заданом масиве  есть строка которая начинается с 2 или 4 мы ее выводим в консоль*************/
+
+
+
+// let array = ["2453", "4000", "65000" , "25860", "908540", "45000", "750004"];
+// let elementArray = array.length;
+
+// function getElemetArray() {
+//   for(let i = 0; i < array.length; i++) {
+//     if( array[i].substring(0,1) === "2" || array[i].substring(0,1) === "4" ) {
+//       console.log(array[i]);
+//     }
+//   }
+// }
+
+// getElemetArray();
+
+
+
+/*Здесь мы создаем функцию, которая будет возвращать true and false, соответственно простое число это или нет и  которая принимает один аргумент, далее с помощю цыкла мы перебираем числа от 2 до num, но так как предварительно мы задали корень квадратный от num, то нам не приходится перебират все числа до num, и если мы находим корень квадратный то число уже не евляется простым */
+function isPrime(num) {
+  for (var i = 2, max = Math.sqrt(num); i <= max; i++) {
+    if (num % i ===  0) {
+      return false;
+    }
+  }
+  return num >= 2;
+}
+/**************  Здесь мы создаем цыкл внутри функции, который перебирает нам все числа до заданого n, а в условие пишем предыдущую функцию,котороя проверяет простое это число или нет и соответсвенно если условия выполняется то выводит это число в консоль *************/
+function getPrime(num) {
+
+  for (let i = 2; i <= num; i++) {
+    if (isPrime(i)) {
+      console.log(`${i}: Делители этого числа: 1 и ${i}`);;
+    }
   }
 }
-stringsMake(" Ghhdfbdsfdf sdhfsbdfjshd shjsbdjfdshfbsdfbv равраываиывраыр ррапвырвнни вравырпаврапвыарирвпаы");
-*/
+console.log(getPrime(100));
